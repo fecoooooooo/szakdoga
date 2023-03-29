5c238e873d3c430877982e9493f774b4312773a9
Ferenc Dajka <fesath_3@hotmail.com>
2023-03-29 12:12:42 +0200

    !A aspnet identity tables

---
+++ Registry-Backend/Registry-Backend/Models/Device.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Registry_Backend.Models;
+
+public partial class Device
+{
+    public int Id { get; set; }
+
+    public string SerialNumber { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public string? Link { get; set; }
+
+    public string? UserId { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public virtual AspNetUser? User { get; set; }
+}
--- Registry-Backend/Registry-Backend/Models/RegistryContext.cs
+++ Registry-Backend/Registry-Backend/Models/RegistryContext.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Registry_Backend.Models;
 
-public partial class RegistryContext : IdentityDbContext 
+public partial class RegistryContext : DbContext
 {
     public RegistryContext()
     {
@@ -16,6 +15,22 @@ public partial class RegistryContext : IdentityDbContext
     {
     }
 
+    public virtual DbSet<ApplicationLog> ApplicationLogs { get; set; }
+
+    public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
+
+    public virtual DbSet<AspNetRoleClaim> AspNetRoleClaims { get; set; }
+
+    public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
+
+    public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
+
+    public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
+
+    public virtual DbSet<AspNetUserToken> AspNetUserTokens { get; set; }
+
+    public virtual DbSet<Device> Devices { get; set; }
+
     public virtual DbSet<Sample> Samples { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -24,8 +39,102 @@ public partial class RegistryContext : IdentityDbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-		base.OnModelCreating(modelBuilder);
-		modelBuilder.Entity<Sample>(entity =>
+        modelBuilder.Entity<ApplicationLog>(entity =>
+        {
+            entity.ToTable("ApplicationLog");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Level).HasMaxLength(50);
+            entity.Property(e => e.Logger).HasMaxLength(250);
+            entity.Property(e => e.MachineName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<AspNetRole>(entity =>
+        {
+            entity.HasIndex(e => e.NormalizedName, "RoleNameIndex")
+                .IsUnique()
+                .HasFilter("([NormalizedName] IS NOT NULL)");
+
+            entity.Property(e => e.Name).HasMaxLength(256);
+            entity.Property(e => e.NormalizedName).HasMaxLength(256);
+        });
+
+        modelBuilder.Entity<AspNetRoleClaim>(entity =>
+        {
+            entity.HasIndex(e => e.RoleId, "IX_AspNetRoleClaims_RoleId");
+
+            entity.HasOne(d => d.Role).WithMany(p => p.AspNetRoleClaims).HasForeignKey(d => d.RoleId);
+        });
+
+        modelBuilder.Entity<AspNetUser>(entity =>
+        {
+            entity.HasIndex(e => e.NormalizedEmail, "EmailIndex");
+
+            entity.HasIndex(e => e.NormalizedUserName, "UserNameIndex")
+                .IsUnique()
+                .HasFilter("([NormalizedUserName] IS NOT NULL)");
+
+            entity.Property(e => e.Email).HasMaxLength(256);
+            entity.Property(e => e.NormalizedEmail).HasMaxLength(256);
+            entity.Property(e => e.NormalizedUserName).HasMaxLength(256);
+            entity.Property(e => e.UserName).HasMaxLength(256);
+
+            entity.HasMany(d => d.Roles).WithMany(p => p.Users)
+                .UsingEntity<Dictionary<string, object>>(
+                    "AspNetUserRole",
+                    r => r.HasOne<AspNetRole>().WithMany().HasForeignKey("RoleId"),
+                    l => l.HasOne<AspNetUser>().WithMany().HasForeignKey("UserId"),
+                    j =>
+                    {
+                        j.HasKey("UserId", "RoleId");
+                        j.ToTable("AspNetUserRoles");
+                        j.HasIndex(new[] { "RoleId" }, "IX_AspNetUserRoles_RoleId");
+                    });
+        });
+
+        modelBuilder.Entity<AspNetUserClaim>(entity =>
+        {
+            entity.HasIndex(e => e.UserId, "IX_AspNetUserClaims_UserId");
+
+            entity.HasOne(d => d.User).WithMany(p => p.AspNetUserClaims).HasForeignKey(d => d.UserId);
+        });
+
+        modelBuilder.Entity<AspNetUserLogin>(entity =>
+        {
+            entity.HasKey(e => new { e.LoginProvider, e.ProviderKey });
+
+            entity.HasIndex(e => e.UserId, "IX_AspNetUserLogins_UserId");
+
+            entity.HasOne(d => d.User).WithMany(p => p.AspNetUserLogins).HasForeignKey(d => d.UserId);
+        });
+
+        modelBuilder.Entity<AspNetUserToken>(entity =>
+        {
+            entity.HasKey(e => new { e.UserId, e.LoginProvider, e.Name });
+
+            entity.HasOne(d => d.User).WithMany(p => p.AspNetUserTokens).HasForeignKey(d => d.UserId);
+        });
+
+        modelBuilder.Entity<Device>(entity =>
+        {
+            entity.ToTable("Device");
+
+            entity.HasIndex(e => e.SerialNumber, "IX_Device").IsUnique();
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Description).HasMaxLength(250);
+            entity.Property(e => e.Link).HasMaxLength(250);
+            entity.Property(e => e.Name).HasMaxLength(250);
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.SerialNumber).HasMaxLength(50);
+            entity.Property(e => e.UserId).HasMaxLength(450);
+
+            entity.HasOne(d => d.User).WithMany(p => p.Devices)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK_Device_AspNetUsers");
+        });
+
+        modelBuilder.Entity<Sample>(entity =>
         {
             entity
                 .HasNoKey()
