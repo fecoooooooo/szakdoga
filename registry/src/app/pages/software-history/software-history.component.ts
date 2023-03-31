180e01258c1e2f8640b4793d53c929e4cd138c74
Ferenc Dajka <fesath_3@hotmail.com>
2023-03-31 18:18:17 +0200

    works hisotry view

--- registry/src/app/pages/software-history/software-history.component.ts
+++ registry/src/app/pages/software-history/software-history.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute, Router } from '@angular/router';
+import {
+  IdentityUser,
+  Software,
+  SoftwareHistory,
+  SoftwaresService,
+  UsersService,
+} from 'api-clients/api';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-software-history',
   templateUrl: './software-history.component.html',
-  styleUrls: ['./software-history.component.scss']
+  styleUrls: ['./software-history.component.scss'],
 })
 export class SoftwareHistoryComponent {
+  displayedColumns: string[] = [
+    'id',
+    'user',
+    'software',
+    'startDate',
+    'endDate',
+  ];
+  dataSource = new MatTableDataSource<Software>();
 
+  users: IdentityUser[] | null = null;
+  softwares: Software[] | null = null;
+
+  constructor(
+    private softwaresService: SoftwaresService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private usersService: UsersService
+  ) {}
+
+  ngOnInit(): void {
+    let id: string | null = this.route.snapshot.paramMap.get('id');
+
+    forkJoin({
+      history: this.softwaresService.apiSoftwaresHistoryForSoftwareIdGet(+id!),
+      users: this.usersService.allUsersGet(),
+      softwares: this.softwaresService.apiSoftwaresAllSoftwaresGet(),
+    }).subscribe((result) => {
+      this.dataSource = new MatTableDataSource<SoftwareHistory>(result.history);
+      this.users = result.users;
+      this.softwares = result.softwares;
+    });
+  }
+
+  getUserName(userId: string) {
+    return this.users?.find((x) => x.id === userId)?.userName;
+  }
+
+  getSoftwareName(softwareId: number) {
+    return this.softwares?.find((x) => x.id === softwareId)?.name;
+  }
+
+  cancel() {
+    this.router.navigate([`/manage-softwares`]);
+  }
 }
